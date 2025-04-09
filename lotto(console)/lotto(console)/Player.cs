8c78221e26165ab2cd9b5da6c4d07b16f1402919
LaszloTorok <162968332+szt3torlas@users.noreply.github.com>
2025-04-09 12:03:34 +0200

    Add files via upload

---
+++ lotto(console)/lotto(console)/Player.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lotto_console_
+{
+    public class Player
+    {
+        public string Name {  get; set; }
+        public string Email { get; set; }
+        public int Amount {  get; set; }
+
+        public Player(string name, string email, int amount)
+        {
+            if (string.IsNullOrEmpty(name)) throw new ArgumentNullException("The player MUST have a Name");
+            else Name = name;
+            if (email.Contains('@') && email.Contains('.')) Email = email;
+            else throw new ArgumentException("The email MUST have the right formate");
+
+            string DoesHaveMoney()
+            {
+                if (amount <= 0) return "The player does not have money to play";
+                else return "The player has money to play";
+            }
+            string MoneyOfPlayer()
+            {
+                return $"{amount} Ft";
+            }
+            void AddMoney(int addamount)
+            {
+                if (amount <= 0) throw new ArgumentException("You MUST add more money than zero!");
+                else amount += addamount;
+            }
+            void Bet(int betamount)
+            {
+                if (betamount > amount) throw new ArgumentException("The bet BIGGER than the account");
+                else amount -= betamount;
+            }
+        }
+        public List<Player> PlayersHavingMoney(List<Player> PlayerList)
+        {
+            List<Player> list = new List<Player>();
+            foreach (var item in PlayerList)
+            {
+                if (item.Amount > 0) list.Add(item);
+            }
+            return list;
+        }
+        public List<Player> PlayersHavingTMoreMoneyThanThis(List<Player> PlayerList, int limit)
+        {
+            List<Player> list = new List<Player>();
+            foreach (var item in PlayerList)
+            {
+                if (item.Amount > limit) list.Add(item);
+            }
+            return list;
+        }/*
+        public int PaidMoneyToCompany(List<Player> PlayerList)
+        {
+            int money = 0;
+            List<Player> list = new List<Player>();
+            foreach (var item in PlayerList)
+            {
+                money
+            }
+            return list;
+        }*/
+    }
+}
+
---
+++ lotto(console)/lotto(console)/Program.cs
@@ -0,0 +1,9 @@
+﻿using lotto_console_;
+
+List<Player> Players = new List<Player>
+{
+    new Player("csávó", "@.", 12200),
+    new Player("", "@.", 12200),
+    new Player("csávó", "@.", 12200),
+    new Player("csávó", "@.", 12200)
+};
